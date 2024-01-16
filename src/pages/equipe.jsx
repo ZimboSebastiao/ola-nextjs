7f3ce8afc1971034a3b3a870765ffeb3f2a79679
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-01-16 11:57:42 -0300

    update

---
+++ src/pages/equipe.jsx
@@ -0,0 +1,19 @@
+import Menu from "@/components/Menu";
+
+export default function Equipe() {
+  return (
+    <>
+      <h1>Olá Next.js🐱‍💻😎!</h1>
+      <Menu />
+      <main>
+        <h2> Equipe</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+          doloremque, officia nobis sequi velit, consectetur sit, nulla alias
+          cum voluptates quibusdam corrupti odio sapiente. Numquam adipisci qui
+          porro architecto quod.
+        </p>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/servicos.jsx
@@ -0,0 +1,18 @@
+import Menu from "@/components/Menu";
+
+export default function Servicos() {
+  return (
+    <>
+      <h1>Olá Next.js🐱‍💻😎!</h1>
+      <Menu />
+      <main>
+        <h2> Servicos</h2>
+        <ol>
+          <li>Bla Bla Bla</li>
+          <li>Bla Bla Bla</li>
+          <li>Bla Bla Bla</li>
+        </ol>
+      </main>
+    </>
+  );
+}
