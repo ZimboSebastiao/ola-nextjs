2c3722c70575cea0354b931fff6b60984916e8e3
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-01-16 11:48:11 -0300

    Exemplo geral de pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Olá Next.js🐱‍💻😎!</h1>
+      <Menu />
+      <main>
+        <h2>Bem-Vindo ao Next.js</h2>
+        <h3>Página inicial</h3>
+      </main>
+    </>
+  );
+}
